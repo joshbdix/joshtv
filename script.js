776d221bf439c3d2199432b5e8f17976618997eb
JOSH•RAS <56694020+joshbdix@users.noreply.github.com>
2026-06-03 02:55:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -278,3 +278,64 @@ setInterval(()=>{
     showLivePopup();
 
 },30000);
+
+
+//Fifa Coundown
+const fifaDate =
+new Date("June 11, 2026 00:00:00").getTime();
+
+setInterval(() => {
+
+const now =
+new Date().getTime();
+
+const distance =
+fifaDate - now;
+
+const days =
+Math.floor(
+distance / (1000 * 60 * 60 * 24)
+);
+
+const hours =
+Math.floor(
+(distance %
+(1000 * 60 * 60 * 24))
+/
+(1000 * 60 * 60)
+);
+
+const minutes =
+Math.floor(
+(distance %
+(1000 * 60 * 60))
+/
+(1000 * 60)
+);
+
+const seconds =
+Math.floor(
+(distance %
+(1000 * 60))
+/
+1000
+);
+
+document.getElementById("days").innerText =
+days;
+
+document.getElementById("hours").innerText =
+hours;
+
+document.getElementById("minutes").innerText =
+minutes;
+
+document.getElementById("seconds").innerText =
+seconds;
+
+},1000);
+
+function closeBanner(){
+    document.getElementById("fifaBanner").style.display="none";
+}
+
